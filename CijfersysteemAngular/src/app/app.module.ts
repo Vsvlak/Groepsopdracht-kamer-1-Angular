81aed683fe4051b2f6a39a0eeb0b4fe0b897ec55
vsvlak <vsvlak@gmail.com>
2020-08-21 16:27:40 +0200

    eerste opzet Angular

---
+++ CijfersysteemAngular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TabelComponent } from './homepage/tabel/tabel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    TabelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
