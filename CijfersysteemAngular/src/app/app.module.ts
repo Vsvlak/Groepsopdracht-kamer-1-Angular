d410197e1081ee463c9f9ce2aa7d7df155705b50
Martijn <martijn@greenhighway.nl>
2020-08-26 11:25:56 +0200

    Design improvements

--- CijfersysteemAngular/src/app/app.module.ts
+++ CijfersysteemAngular/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { LeerlingTabelComponent } from './homepage/leerlingtabel/leerlingtabel.component';
-import { HttpClientModule} from '@angular/common/http'
+import { LeerlingTabelComponent } from './components/leerlingtabel/leerlingtabel.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ViewLeerlingenPageComponent } from './view-leerlingen-page/view-leerlingen-page.component'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HttpClientModule} from '@angular/common/http'
     HeaderComponent,
     FooterComponent,
     HomepageComponent,
-    LeerlingTabelComponent
+    LeerlingTabelComponent,
+    ViewLeerlingenPageComponent
   ],
   imports: [
     BrowserModule,
