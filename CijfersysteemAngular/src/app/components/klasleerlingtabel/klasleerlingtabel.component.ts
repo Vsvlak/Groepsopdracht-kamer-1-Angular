46e0642ab1a64958ed38268efab687ed13310cfc
Lisia <lisia.smienk@ziggo.nl>
2020-09-02 10:36:17 +0200

    koppeling klas-leerling

--- CijfersysteemAngular/src/app/components/klasleerlingtabel/klasleerlingtabel.component.ts
+++ CijfersysteemAngular/src/app/components/klasleerlingtabel/klasleerlingtabel.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { LeerlingDto } from 'src/app/models/LeerlingDto';
-import { LeerlingService } from 'src/app/service/leerling.service';
-import { KlasService } from 'src/app/service/klas.service';
+import { LeerlingDto } from '../../models/LeerlingDto';
+import { LeerlingService } from '../../service/leerling.service';
+import { KlasService } from '../../service/klas.service';
 import { ActivatedRoute } from '@angular/router';
-import { ViewLeerlingenPageComponent } from 'src/app/view-leerlingen-page/view-leerlingen-page.component';
-
+import { KlasLeerlingDto } from '../../models/KlasLeerlingDto';
 
 @Component({
   selector: 'app-klasleerlingtabel',
@@ -13,44 +12,40 @@ import { ViewLeerlingenPageComponent } from 'src/app/view-leerlingen-page/view-l
 })
 export class KlasleerlingtabelComponent implements OnInit {
 
-leerling: LeerlingDto;
-leerlingen: LeerlingDto[];
-leerlingenDrop: LeerlingDto[];
-ks: KlasService;
+  leerling: LeerlingDto;
+  leerlingen: LeerlingDto[];
+  leerlingenDrop: LeerlingDto[];
+  klasleerling: KlasLeerlingDto = {
+    klasid : undefined,
+    leerlingid : undefined
+  }
 
-  constructor(private ls: LeerlingService, private activatedrouter: ActivatedRoute) { }
+  constructor(private ks: KlasService, private ls: LeerlingService, private activatedrouter: ActivatedRoute) { }
 
   ngOnInit(): void {
-
-    this.activatedrouter.paramMap.subscribe(params =>{
+    this.maakLeerlingenDropdown();
+    this.activatedrouter.paramMap.subscribe(params => {
       this.ls.findLeerlingenVanKlas(Number.parseInt(params.get("klasid"))).subscribe(leerlingenlijst => {
         this.leerlingen = leerlingenlijst;
       });
     })
-
   }
 
-
-  maakLeerlingenDropdown(){ 
-      this.ls.findLeerlingen().subscribe(leerlingenlijst2 => {
-        this.leerlingenDrop = leerlingenlijst2;
-        for (let i = 0 ; i < leerlingenlijst2.length; i++){ 
-          document.getElementById("kiesleerling").innerHTML += "<option>" + leerlingenlijst2[i].id + ". " + leerlingenlijst2[i].voornaam + " " + leerlingenlijst2[i].achternaam + "</option>";
-        }
-      });
+  maakLeerlingenDropdown() {
+    this.ls.findLeerlingen().subscribe(leerlingenlijst2 => {
+      this.leerlingenDrop = leerlingenlijst2;
+      for (let i = 0; i < leerlingenlijst2.length; i++) {
+        document.getElementById("kiesleerling").innerHTML += "<option>" + leerlingenlijst2[i].id + ". " + leerlingenlijst2[i].voornaam + " " + leerlingenlijst2[i].achternaam + "</option>";
+      }
+    });
   }
 
-  /*voegLeerlingToe(){ 
-   var NieuweLeerlingInKlas = (<HTMLInputElement> document.getElementById('kiesleerling')).value;
-   var idNieuweLeerlingInKlas =  NieuweLeerlingInKlas.split(".")[0];
-   for(var i=0; i<this.leerlingenDrop.length; i++){
-     if (this.leerlingenDrop[i].id == parseInt(idNieuweLeerlingInKlas)){
-        this.leerling = this.leerlingenDrop[i];      
-        this.ks.voegLeerlingToeService(leerling)).subscribe((leerling) => 
-        alert(leerling));
-     }
-   }
-   
-  }*/
-
+  voegLeerlingToe() {
+    var leerlingID = (<HTMLInputElement>document.getElementById('kiesleerling')).value.split(".")[0];
+    this.klasleerling.leerlingid = Number.parseInt(leerlingID);
+    this.activatedrouter.paramMap.subscribe(params => {
+      this.klasleerling.klasid = Number.parseInt(params.get("klasid"));
+    })
+    this.ks.voegLeerlingToeService(this.klasleerling);   
+  }
 } 
